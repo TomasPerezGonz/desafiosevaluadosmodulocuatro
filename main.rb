65dd70d5f0e535e8b37d46a94a45e4cc11e8d01b
TomasPerezGonz <tomasperezg@gmail.com>
2023-06-23 16:15:37 +0000

    Actualizar archivos

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
-puts 'Hello, World!'
+def menu(n)
+  n.times do |i|
+    if i == 0
+      puts "Menú de opciónes"
+    elsif i < n - 1
+      puts "#{i}) Opción #{i}"
+    elsif i == n - 1
+      puts "#{i}) Salir"
+    end
+  end
+  puts "Selecciona tu opción: "
+end
+
+menu(5)
+
