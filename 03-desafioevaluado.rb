ec036e27b418646ded5e08e636a67cbe1696beae
TomasPerezGonz <tomasperezg@gmail.com>
2023-06-23 20:10:43 +0000

    Ejercicio 1 completo

--- 03-desafioevaluado.rb
+++ 03-desafioevaluado.rb
@@ -1,3 +1,5 @@
+#Ejercicio 1
+
 #Se solicita ingresar mediante metodo puts el salario del trabajador
 puts "Ingrese el salario actual del trabajador: "
 #Se crea variable sal que pide al usuario el salario actual y lo transforma en integer
@@ -7,6 +9,7 @@ puts "Ingrese el porcentaje de aumento: "
 #Se crea variable aum que pide al usuario el porcentaje de aumento y lo transforma en float
 aum = gets.chomp.to_f
 
+#Se utiliza while para fijar el minimo de aumento en un 5%, si no se cumple esta condición se solicita volver a ingresar el valor
 while aum < 5.0
   puts "El porcentaje minimo de aumento es de 5%, porfavor ingresa un porcentaje valido:"
   aum = gets.chomp.to_f
@@ -23,7 +26,7 @@ def nuevoSalario(x, y)
   puts " El aumento es de $#{aumento.round(0)}.\n El nuevo salario asciende a $#{salarioFinal.round(0)}"
 end
 
-
-
 #Se llama al metodo nuevoSalario con las 2 variables solicitadas por consola.
 nuevoSalario(sal, aum)
+
+#Ejercicio 2
