91fb8b98edbcf524a8d61cbe6a86c2cec86e9caa
TomasPerezGonz <tomasperezg@gmail.com>
2023-06-23 21:06:29 +0000

    Ejercicio 2 completo

--- 03-desafioevaluado.rb
+++ 03-desafioevaluado.rb
@@ -30,3 +30,56 @@ end
 nuevoSalario(sal, aum)
 
 #Ejercicio 2
+
+#Se llama a la biblioteca securerandom
+require 'securerandom'
+
+#Se solicita al usuario seleccionar un rango de juego
+puts "Selecciona el numero máximo para jugar"
+rango = gets.chomp.to_i
+
+#A través del while se verifica que la opción sea valida
+while rango.class != Integer
+  puts "El valor seleccionado no es valido"
+  rango = gets.chomp.to_i
+end
+
+#Se crea la variable computador que seleccionará con SecureRandom un numero entre 1 y el valor de la variable rango
+computador = SecureRandom.rand(1..rango)
+
+#Se define el metodo def nombrandolo juego que tomará el parametro rango
+def juego(rango)
+  #Se genera el punto de loop para volver a jugar
+  loop do
+    #Se solicita al usuario agregar un numero para adivinar cual fue seleccionado por el computador
+  puts "Adivina el valor:"
+  usuario = gets.chomp.to_i
+    #Se utiliza until para que el usuario ingrese un valor entre 1 y el valor seleccionado en la variable rango recordandole la opción por medio de un puts dejandolo volver a ingresar un numero
+     until usuario >= rango && usuario > 0
+        puts "Ingresaste un numero incorrecto, recuerda que seleccionaste #{rango}, porfavor vuelve a seleccionar un numero:"
+        usuario = gets.chomp.to_i
+     end
+    #Se utiliza while para comparar el numero arrojado por random y el elejido por el usuario y se consulta si quiere volver a jugar
+  while rango != usuario
+    puts "Te has equivocado, ¿Quieres vovler a intentarlo? (s/n)"
+    opcion = gets.chomp.downcase
+    #Se utiliza until para que el usuario utilice las opciones s o n para seguir jugando o terminar el juego
+    until opcion == "s" || opcion == "n"
+      puts "Debes ingresar una opcion valida"
+          opcion = gets.chomp.downcase
+    end
+    #Si el usuario ingresa s el proggrama lo deja ingresar un nuevo valor para continuar el juego
+    if opcion == "s"
+      puts "Ingresa un nuevo valor: "
+      usuario = gets.chomp.to_i
+    end
+    #return unless devolverá al inicio del metodo a menos que el usuario seleccione que no quiere seguir jugando
+      return unless opcion == "s"
+  end
+  end
+end
+  
+
+  
+#Se llama la función utilizando el valor de la variable rango definida con SecureRandom
+  juego(rango)
